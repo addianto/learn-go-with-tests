2e80dcdd48e0214d56c8160c6241b9c09d9cb0d5
Daya Adianto <addianto@users.noreply.github.com>
2025-02-02 00:20:11 +0700

    Add assertion function in the unit test

--- hello-world/reading/hello_test.go
+++ hello-world/reading/hello_test.go
@@ -5,16 +5,19 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("given a name, expect greeting", func(t *testing.T) {
 		got := Hello("Alice")
-		want := "Hello, Alice"
-		if got != want {
-			t.Errorf("got %q; want %q", got, want)
-		}
+		want := "Hello, Bob"
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("given empty name, expect default greeting", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-		if got != want {
-			t.Errorf("got %q; want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q; want %q", got, want)
+	}
 }
