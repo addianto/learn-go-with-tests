c51cee0f7d2ca40d35e1d3f57f605272cab82b77
Daya Adianto <addianto@users.noreply.github.com>
2025-02-02 00:03:49 +0700

    Handle empty name parameter

--- hello-world/reading/hello.go
+++ hello-world/reading/hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		return englishHelloPrefix + "world"
+	}
 	return englishHelloPrefix + name
 }
 
--- hello-world/reading/hello_test.go
+++ hello-world/reading/hello_test.go
@@ -3,10 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Bob")
-	want := "Hello, Bob"
-
-	if got != want {
-		t.Errorf("got %q; want %q", got, want)
-	}
+	t.Run("given a name, expect greeting", func(t *testing.T) {
+		got := Hello("Alice")
+		want := "Hello, Alice"
+		if got != want {
+			t.Errorf("got %q; want %q", got, want)
+		}
+	})
+	t.Run("given empty name, expect default greeting", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		if got != want {
+			t.Errorf("got %q; want %q", got, want)
+		}
+	})
 }
