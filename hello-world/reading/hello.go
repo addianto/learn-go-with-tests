8c738b6a1739d4e75d5c67089397f8ce973dcafc
Daya Adianto <addianto@users.noreply.github.com>
2025-02-02 15:39:28 +0700

    Add Spanish variant

--- hello-world/reading/hello.go
+++ hello-world/reading/hello.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
-		return englishHelloPrefix + "world"
+		return englishHelloPrefix + "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", ""))
 }
--- hello-world/reading/hello_test.go
+++ hello-world/reading/hello_test.go
@@ -4,13 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("given a name, expect greeting", func(t *testing.T) {
-		got := Hello("Alice")
-		want := "Hello, Bob"
+		got := Hello("Alice", "")
+		want := "Hello, Alice"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("given empty name, expect default greeting", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, world"
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
